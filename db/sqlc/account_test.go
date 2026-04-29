4539a2da100ab8809d31fefb10b56d4929d018fd
Khanh Duy <21521995@gm.uit.edu.vn>
2026-04-29 23:29:45 +0700

    units test for accounts

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,16 +2,19 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
+	"time"
 
+	"github.com/lh-khanhduy/banco_de_rata/utils"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	args := CreateAccountParams{
-		Owner:    "crowley",
-		Balance:  666,
-		Currency: "USD",
+		Owner:    utils.RandomOwner(),
+		Balance:  utils.RandomMoney(),
+		Currency: utils.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), args)
@@ -25,4 +28,82 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+// <-- --------------------------***----------------------- -->
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account := createRandomAccount(t)
+
+	found, err := testQueries.GetAccount(context.Background(), account.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, found)
+
+	require.Equal(t, found, account)
+	require.WithinDuration(t, account.CreatedAt, found.CreatedAt, time.Second)
+
+}
+
+func TestUpdateAccount(t *testing.T) {
+	originalAcc := createRandomAccount(t)
+
+	updateArgs := UpdateAccountParams{
+		ID:      originalAcc.ID,
+		Balance: utils.RandomMoney(),
+	}
+
+	err := testQueries.UpdateAccount(context.Background(), updateArgs)
+
+	updatedAcc, _ := testQueries.GetAccount(context.Background(), originalAcc.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedAcc)
+
+	require.Equal(t, originalAcc.ID, updatedAcc.ID)
+	require.Equal(t, originalAcc.Owner, updatedAcc.Owner)
+	require.NotEqual(t, originalAcc.Balance, updatedAcc.Balance)
+	require.Equal(t, updateArgs.Balance, updatedAcc.Balance)
+	require.Equal(t, originalAcc.Currency, updatedAcc.Currency)
+	require.WithinDuration(t, originalAcc.CreatedAt, updatedAcc.CreatedAt, time.Second)
+
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account := createRandomAccount(t)
+
+	err := testQueries.DeleteAccount(context.Background(), account.ID)
+
+	require.NoError(t, err)
+
+	found, err := testQueries.GetAccount(context.Background(), account.ID)
+
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, found)
+}
+
+func TestListAccounts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+
+	args := ListAccountParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccount(context.Background(), args)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
 }
---
+++ utils/random.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"math/rand"
+	"strings"
+)
+
+const (
+	charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
+
+func randBool() bool {
+	return rand.Intn(2) == 0
+}
+
+func randStringFromSet(set ...string) string {
+	n := len(set)
+	if n == 0 {
+		return ""
+	}
+	return set[rand.Intn(n)]
+}
+
+func randInt(min, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+
+func randFloat64(min float64, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
+
+// RandomString generates a random string of length n.
+func randomString(n int) string {
+	var sb strings.Builder
+	sb.Grow(n)
+	for i := 0; i < n; i++ {
+		sb.WriteByte(charset[rand.Intn(len(charset))])
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return randomString(8)
+}
+
+func RandomMoney() int64 {
+	return int64(randInt(0, 1000))
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
